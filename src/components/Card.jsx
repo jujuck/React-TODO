3cbc972d6b0c6421f42e1cb0c70fc49c0af315b4
Julien RICHARD <julien.y.richard@gmail.com>
2024-04-26 14:37:26 +0200

    Mise en place de la structure de la carte

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Card() {
-  return <div>Card</div>;
+  return (
+    <section>
+      <div className="status">
+        <h4>Status</h4>
+      </div>
+      <div className="description">
+        <h3>Mon titre H3</h3>
+        <div>
+          <button>Modifié </button>
+        </div>
+      </div>
+      <div className="category">
+        <img src="" alt="alt de l'image" />
+        <p>Category</p>
+      </div>
+      <div className="deadline">
+        <p>DeadLine</p>
+      </div>
+      <div className="trash">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/860/860829.png"
+          alt="poubelle"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Card;
