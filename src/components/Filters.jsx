a282352dd1e3ff6bfead59f9bd7d212e1b51f4ae
Julien RICHARD <julien.y.richard@gmail.com>
2024-04-26 14:25:45 +0200

    Mise en place de la strucutre des filtres

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Filters() {
-  return <div>Filters</div>;
+  return (
+    <section className="todo-filter">
+      <label htmlFor="status">
+        Filtrer par statut :
+        <select name="status" id="status">
+          <option value="">Toutes les to do</option>
+          <option value="A faire">A faire</option>
+          <option value="En cours">En cours</option>
+          <option value="En pause">En pause</option>
+        </select>
+      </label>
+      <label htmlFor="description">
+        Filtrer par description :
+        <input type="text" id="description" />
+      </label>
+    </section>
+  );
 }
 
 export default Filters;
