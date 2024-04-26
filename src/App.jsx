3354f1e1afef1749999d798b8e2c6f2b5db72b6b
Julien RICHARD <julien.y.richard@gmail.com>
2024-04-26 14:21:50 +0200

    Création du composant filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Filters from "./components/Filters";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Filters />
       <h1>Ma liste To do</h1>
     </>
   );
